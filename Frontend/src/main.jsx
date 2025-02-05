b1e6a34835499667fd95175d6a227cc9bfea9577
CodeAritraDhank <aridhank21205@gmail.com>
2025-02-05 21:03:46 +0530

    updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AttendanceProvider } from "./context/AttendancePRovider.jsx";
+import { AttendanceProvider } from "./context/AttendanceProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
