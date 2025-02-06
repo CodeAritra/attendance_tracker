44bb75682c4874112a31a524f060d4424d2c8abe
CodeAritraDhank <aridhank21205@gmail.com>
2025-02-06 19:58:07 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -2,25 +2,27 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db/db.js";
-import authRoutes from "./routes/authRoutes.js"
-import routineRoutes from "./routes/routineRoutes.js"
-import attendanceRoutes from "./routes/attendanceRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import routineRoutes from "./routes/routineRoutes.js";
+import attendanceRoutes from "./routes/attendanceRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 // MongoDB Connection
-connectDB()
+connectDB();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.use("/api/auth",authRoutes)
-app.use("/api/routine",routineRoutes)
-app.use("/api/attendance",attendanceRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/routine", routineRoutes);
+app.use("/api/attendance", attendanceRoutes);
 
-
+app.get("/", (req, res) => {
+  res.send("WELCOME TO THE SERVER");
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
