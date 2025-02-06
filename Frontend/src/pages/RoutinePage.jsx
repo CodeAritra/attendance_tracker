4656ba8c5aa22bcb0d4dc8bd0655208ba1725dca
CodeAritraDhank <aridhank21205@gmail.com>
2025-02-06 20:42:03 +0530

    updated

--- Frontend/src/pages/RoutinePage.jsx
+++ Frontend/src/pages/RoutinePage.jsx
@@ -14,7 +14,7 @@ export default function RoutinePage() {
   return (
     <Navigation>
       <div className="bg-gray-50 max-h-screen p-6">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-4xl mx-auto pb-1">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
