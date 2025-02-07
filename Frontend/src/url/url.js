a085fa2694bca2806d1ddf98a2089401c578715a
CodeAritraDhank <aridhank21205@gmail.com>
2025-02-07 13:15:55 +0530

    loader

--- Frontend/src/url/url.js
+++ Frontend/src/url/url.js
@@ -1,2 +1,2 @@
-// export const URL = "https://attendance-tracker-backend-qxyq.onrender.com"
-export const URL = "http://localhost:5000"
+export const URL = "https://attendance-tracker-backend-qxyq.onrender.com"
+// export const URL = "http://localhost:5000"
